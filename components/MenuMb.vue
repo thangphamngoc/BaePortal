f79cae1ba0c2498e594bcbef92def75146b95ae0
thangpn.mc@gmail.com <thangpn.mc@gmail.com>
2021-09-26 02:27:37 +0700

    update

---
+++ components/MenuMb.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <nav class="site-nav js-site-nav" role="navigation" aria-label="navigation">
+    <div class="site-nav__curtain js-nav-curtain"></div>
+    <div class="wrapper wrapper--nav-area">
+      <button
+        class="menu-button js-menu-button"
+        aria-expanded="false"
+        aria-controls="menu"
+        aria-label="Menu button"
+      >
+        <span class="menu-button__lines">
+          <span class="menu-button__line"></span>
+          <span class="menu-button__line"></span>
+          <span class="menu-button__line"></span>
+        </span>
+        Menu
+      </button>
+
+      <ul class="site-nav__menu u-list-reset js-menu" id="menu">
+        <li><a class="site-nav__link" href="#">Irure dolor</a></li>
+        <li><a class="site-nav__link" href="#">Deserunt </a></li>
+        <li><a class="site-nav__link" href="#">Mollit</a></li>
+        <li><a class="site-nav__link" href="#">Est laborum</a></li>
+      </ul>
+      <a class="logo" href="#"> Ut labore. </a>
+    </div>
+  </nav>
+</template>
+
+
+
+
+
+  
